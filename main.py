e3cbcef512377a12663783c589a3d9cdeaaae3fc
Sebastian <sebastianllg@hotmail.com>
2023-06-17 21:18:52 -0500

    inicial

--- main.py
+++ main.py
@@ -9,31 +9,37 @@ chrome_driver = webdriver.Chrome()
 
 bot = TelegramBot()
 db = MongoDB()
+URL="https://www.mercadolibre.com.ec/"
 
-
-chrome_driver.get("https://www.wikipedia.org/")
-print(chrome_driver.title)
+chrome_driver.get(URL)
+#chrome_driver.get("https://www.wikipedia.org/")
+print("----",chrome_driver.title,"-----")
 
 topics = [
-    "quito",
-    "LDU",
-    "Llerena"
+   # "carro",
+  #  "moto",
+    "play station 5"
 ]
 
 for t in topics:
-    search_box = chrome_driver.find_element(By.ID, "searchInput")
+    search_box = chrome_driver.find_element(By.CLASS_NAME, "nav-search-input")
+    #search_box = chrome_driver.find_element(By.ID, "searchInput")
     search_box.send_keys(t)
-    search_button = chrome_driver.find_element(By.CSS_SELECTOR, '#search-form > fieldset > button > i')
+    search_button = chrome_driver.find_element(By.CLASS_NAME, "nav-search-btn")
+    #search_button = chrome_driver.find_element(By.CSS_SELECTOR, '#search-form > fieldset > button > i')
     search_button.click()
 
-    content = chrome_driver.find_element(By.ID, "mw-content-text")
+    content = chrome_driver.find_element(By.CLASS_NAME, "ui-search-layout__item")
+    #content = chrome_driver.find_element(By.ID, "mw-content-text")
     print(content.text)
+    message: str = "Hola como estás\n"
     text = content.text
+    text1= message+text,
     text = text[:4000]
-    bot.send_tg_message(text)
+    bot.send_tg_message(text1)
     db.insert_wikipedia_text(title=t, text=text)
     time.sleep(2)
-    chrome_driver.get("https://www.wikipedia.org/")
+    chrome_driver.get(URL)
 
 
 print("fin")
--- telegram_bot.py
+++ telegram_bot.py
@@ -9,13 +9,16 @@ class TelegramBot:
     def __init__(self):
         self.token = os.getenv("BOT_TOKEN")
         self.admin_tg_user_id = os.getenv("ADMIN_USER_ID")
-
     def send_tg_message(self, text: str):
+        message: str = "Hola como estás"
         url = f"https://api.telegram.org/bot{self.token}/sendMessage"
         data = {
             "chat_id":self.admin_tg_user_id,
+            "message": message,
             #"chat_id": "5120471293",
             "text": text
+
+
         }
 
         return requests.post(url=url, data=data).status_code
